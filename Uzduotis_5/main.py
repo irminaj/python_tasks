1aeabdf02afb93395bd36caa35a3cf08d2100b3e
irminaj <irminaj@github.com>
2022-09-28 23:59:13 +0300

    solution to the fifth task

--- Uzduotis_5/main.py
+++ Uzduotis_5/main.py
@@ -1,6 +1,16 @@
 # Importuokite reikiamus modulius, kad veiktų žemiau nurodytos funkcijos:
 
 # Kitų failų ir žemiau esančio kodo nekeiskite
+
+import modules.num.ints as integers
+from modules.mathematics.addition import add
+from modules.mathematics.division import divide
+from modules.mathematics.subtraction import subtract
+from modules.mathematics.multiplication import multiply
+
+
+
+
 a = add(integers.one, integers.four)
 b = divide(integers.four, integers.two)
 c = subtract(integers.three, integers.two)
